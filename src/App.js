c627b51d6047c86898f9881de15f0cae990553ef
AvaCode69 <reacthmd34@gmail.com>
2023-05-28 00:13:37 +0200

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
+import styled from "styled-components";
+
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth </h4>;
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+
+        <Route path="/about" element={<About />} />
+
+        <Route path="/cart" element={<Cart />} />
+
+        <Route path="/products" element={<Products />} />
+        <Route exact path="/products/:id" element={<SingleProduct />} />
+
+        {/* <Route exact path="/products/:id" children={<SingleProduct />} /> */}
+        <Route path="/checkout" element={<Checkout />} />
+
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
