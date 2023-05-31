8673ffba7d2109e36f113f32a286b92a28868f76
AvaCode69 <reacthmd34@gmail.com>
2023-05-31 11:24:23 +0200

    homePage-contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <section className="section-center">
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className="content">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+          </p>
+          <form>
+            <input
+              type="email"
+              className="form-input"
+              placeholder="enter email"
+            />
+            <button type="button" className="submit-btn">
+              subscribe
+            </button>
+          </form>
+        </div>
+      </section>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +90,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
